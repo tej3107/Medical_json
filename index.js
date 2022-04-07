46f3facd5acf71a8f881ad04628c6d29e0fba95e
Tej Narayan Singh <tejnarayan950@gmail.com>
2022-04-07 13:21:28 +0530

    phase 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello");
+    res.render("home.ejs");
 })
 
 app.listen(process.env.PORT||3000,()=>{
